ef08619dde93bcc71ba9b05e4119bf8924e3228c
Efrain Hernandez <efrainhg2050@gmail.com>
2021-08-25 22:12:59 -0500

    First Array of totos for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { TodoCounter } from './components/TodoCounter/TodoCounter';
+import { TodoItem } from './components/TodoItem/TodoItem';
+import { TodoList } from './components/TodoList/TodoList';
+import { TodoSearch } from './components/TodoSearch/TodoSearch';
+
+const todos = [
+    { id: 1, text: "Ir al cine por palomitas", completed: false},
+    { id: 2, text: "Leer un libro", completed: false},
+    { id: 3, text: "Estudiar un poco más spbre React", completed: false},
+    
+]
 
 export const App = () => {
     return (
-        <div>
-            <h1>To Do Machine App in React</h1>
-        </div>
+        <>
+            <TodoCounter/>
+            <TodoSearch />
+            <TodoList>
+                { todos.map( (todo) => (
+                    <TodoItem 
+                        key={todo.id}
+                        text={todo.text}
+                        completed={todo.completed}
+                    />
+                ))}
+            </TodoList>
+        </>
     )
 }
