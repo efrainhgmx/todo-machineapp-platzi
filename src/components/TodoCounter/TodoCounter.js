9644a378ce6c19a6082043a7b460ccb479d39d76
Efrain Hernandez <efrainhg2050@gmail.com>
2021-08-25 21:46:10 -0500

    Todo counter added

---
+++ src/components/TodoCounter/TodoCounter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const TodoCounter = () => {
+    return (
+        <>
+            <h1>To do Machine App React <span>⚛️</span></h1>
+            <p>Te quedan 2 de 3 tareas por hacer.</p>
+        </>
+    )
+}
