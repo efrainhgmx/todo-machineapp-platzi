e964348bb0041ec74cd138ab2e9a6ca78f131040
Efrain Hernandez <efrainhg2050@gmail.com>
2021-08-31 14:12:18 -0500

    onChange event for input

--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 export const TodoSearch = () => {
-    return (
-        <input className="TodoSearch" type="text" placeholder="Agregar tarea" />
-    )
+ const [searchValue, setSearchValue] = React.useState('');
+  
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  return [
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />,
+    <p>{searchValue}</p>
+  ];
 }
