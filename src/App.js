cdf7a0c6107d299e918322b4447d418e3cf39e58
Efrain Hernandez <efrainhg2050@gmail.com>
2021-08-31 17:52:43 -0500

    Total items for todo counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CreateTodoButton } from './components/CreateTodoButton/CreateTodoButton';
 import { TodoCounter } from './components/TodoCounter/TodoCounter';
 import { TodoItem } from './components/TodoItem/TodoItem';
 import { TodoList } from './components/TodoList/TodoList';
 import { TodoSearch } from './components/TodoSearch/TodoSearch';
 
-const todos = [
+const defaultTodos = [
     { id: 1, text: "Ir al cine por palomitas", completed: false},
     { id: 2, text: "Leer un libro", completed: false},
     { id: 3, text: "Estudiar un poco más spbre React", completed: false},
@@ -13,10 +13,32 @@ const todos = [
 ]
 
 export const App = () => {
+    const [todos, setTodos] = useState(defaultTodos);
+    const [searchValue, setSearchValue] = usaState("");
+
+    const completedTodos = todos.filter( todo => !!todo.completed);
+    const totalTodos = todos.length;
+
+    let searchedTodos = [];
+
+    if(!searchedTodos.length > 1) {
+        searchedTodos = todos;
+    } else {
+        searchedTodos = todos.filter(todo => {
+            const todoText = todo.text.toLowerCase();
+            const searchText = searchValue.toLowerCase();
+            return todoText.includes(searchText);
+        });
+    }
+
     return (
         <>
-            <TodoCounter/>
-            <TodoSearch />
+            <TodoCounter 
+            total={totalTodos}
+            completed={completedTodos}/>
+            <TodoSearch
+            searchValue={searchValue}
+            setSearchValue={setSearchValue} />
             <TodoList>
                 { todos.map( (todo) => (
                     <TodoItem 
--- src/components/TodoCounter/TodoCounter.js
+++ src/components/TodoCounter/TodoCounter.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './style.css';
 
-export const TodoCounter = () => {
+export const TodoCounter = ({total, completed}) => {
     return (
         <>
             <h1>To do Machine App React <span>⚛️</span></h1>
-            <p>Te quedan 2 de 3 tareas por hacer.</p>
+            <p>Haz completado {completed} tareas de {total}.</p>
         </>
     )
 }
